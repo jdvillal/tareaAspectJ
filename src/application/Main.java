fb8d82c2a7319db90721afe36618ae3186f09d34
unknown <daniel.--96@hotmail.com>
2020-11-05 22:57:25 -0800

    Se ha creado el proyecto y se lo ha trasnformado a AspectJ project

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			GridPane root = new GridPane();
+			Button btn1 = new Button("Rojo");
+			Button btn2 = new Button("Verde");
+			Button btn3 = new Button("Azul");
+			Scene scene = new Scene(root, 600, 600);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
